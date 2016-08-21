91143c6c98c2cb856cbad375a14acd209a02b4a2
Jonathan Daugherty <cygnus@foobox.com>
2016-08-21 15:06:49 -0700

    Add a routine for setting markup regions using a tree structure

--- programs/MarkupTest.hs
+++ programs/MarkupTest.hs
@@ -1,10 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import qualified Data.Text as T
 import Data.Text.Markup
 
 main :: IO ()
 main = do
     let t = "this is some text"
-        val = ()
-    print $ fromMarkup $ toMarkup t val
+        val :: Int
+        val = 1
+        m0 = toMarkup t val
+        -- Set "this " to 2
+        m1 = markRegion 0 5 2 m0
+        -- Set "his" to 3
+        m2 = markRegion 1 3 3 m1
+        -- Set "is is so" to 4
+        m3 = markRegion 2 8 4 m2
+        -- Set "is" to 5
+        m4 = markRegion 2 2 5 m3
+    print $ fromMarkup m0
+    print $ fromMarkup m1
+    print $ fromMarkup m2
+    print $ fromMarkup m3
+    print $ fromMarkup m4
--- src/Data/Text/Markup.hs
+++ src/Data/Text/Markup.hs
@@ -2,28 +2,26 @@ module Data.Text.Markup
   ( Markup
   , toMarkup
   , fromMarkup
+  , markRegion
   )
 where
 
 import Data.Monoid ((<>))
 import qualified Data.Text as T
-import Data.Tree
 
-data Descriptor a =
-   Desc { seqStart :: Int
-        , seqLength :: Int
-        , seqData :: a
-        }
-        deriving (Show)
+data SequenceTree a =
+    Node Int Int [SequenceTree a]
+    | Leaf Int Int a
+    deriving (Show)
 
 data Markup a =
     Markup { sourceText :: T.Text
-           , markupMapping :: Tree (Descriptor a)
+           , markupMapping :: SequenceTree a
            }
            deriving (Show)
 
 toMarkup :: T.Text -> a -> Markup a
-toMarkup t a = Markup t (Node (Desc 0 (T.length t) a) [])
+toMarkup t a = Markup t (Leaf 0 (T.length t) a)
 
 fromMarkup :: Markup a -> [(T.Text, a)]
 fromMarkup (Markup txt tree) =
@@ -32,14 +30,58 @@ fromMarkup (Markup txt tree) =
     let descs = leaves tree
         initialState :: (T.Text, [(T.Text, a)])
         initialState = (txt, [])
-        nextChunk desc (remainingText, prevChunks) =
-            let (thisText, remainingText') = T.splitAt (seqLength desc) remainingText
-                thisChunk = (thisText, thisValue)
-                thisValue = seqData desc
+        nextChunk (remainingText, prevChunks) (_, len, val) =
+            let (thisText, remainingText') = T.splitAt len remainingText
+                thisChunk = (thisText, val)
             in (remainingText', prevChunks <> [thisChunk])
-        (_, chunks) = foldr nextChunk initialState descs
+        (_, chunks) = foldl nextChunk initialState descs
     in chunks
 
-leaves :: Tree a -> [a]
-leaves (Node l []) = [l]
-leaves (Node _ cs) = concat $ leaves <$> cs
+markRegion :: (Show a) => Int -> Int -> a -> Markup a -> Markup a
+markRegion start len val (Markup txt t0) = Markup txt t1
+    where
+        t1 = treeMarkRegion start len val t0
+
+-- Need recursive algorithm to rebuild tree with nodes split up as
+-- necessary
+treeMarkRegion :: (Show a) => Int -> Int -> a -> SequenceTree a -> SequenceTree a
+treeMarkRegion newStart newLen newVal leaf@(Leaf lStart lLen oldVal) =
+    if not (startInLeaf || endInLeaf) then leaf
+    else if length validLeaves == 1
+         then validLeaves !! 0
+         else if length validLeaves > 1
+              then Node lStart lLen validLeaves
+              else error "Ended up with zero valid leaves!"
+    where
+        end = newStart + newLen
+        lEnd = lStart + lLen
+        startInLeaf = newStart >= lStart && newStart <= lEnd
+        endInLeaf = end >= lStart && end <= lEnd
+
+        -- Clamp the new node leaf to the size of the current leaf since
+        -- the request could be larger than this leaf
+        newStart' = max lStart newStart
+        newEnd = min lEnd (newStart + newLen)
+        newLen' = newEnd - newStart'
+
+        newLeaves = [ Leaf lStart (newStart - lStart) oldVal
+                    , Leaf newStart' newLen' newVal
+                    , Leaf newEnd (lEnd - newEnd) oldVal
+                    ]
+        validLeaves = filter isValidLeaf newLeaves
+        isValidLeaf (Leaf _ l _) = l > 0
+
+treeMarkRegion start len newVal node@(Node lStart lLen cs) =
+    let end = start + len
+        lEnd = lStart + lLen
+        startInNode = start >= lStart && start <= lEnd
+        endInNode   = end   >= lStart && end   <= lEnd
+    -- If the start or end is somewhere in this node, we need to process
+    -- the children
+    in if startInNode || endInNode
+       then Node lStart lLen $ treeMarkRegion start len newVal <$> cs
+       else node
+
+leaves :: SequenceTree a -> [(Int, Int, a)]
+leaves (Leaf st len a) = [(st, len, a)]
+leaves (Node _ _ cs) = concat $ leaves <$> cs
