dfd9fbeaa9537ef9d426700bf983196d72809c59
Jonathan Daugherty <cygnus@foobox.com>
2016-08-21 20:25:59 -0700

    Add some quickcheck properties

--- tests/Main.hs
+++ tests/Main.hs
@@ -3,12 +3,26 @@ module Main where
 
 import Test.Tasty as T
 import Test.Tasty.QuickCheck as T
+import Test.QuickCheck.Property
+-- For Arbitrary instance for Text
+import Data.Text.Arbitrary ()
+import qualified Data.Text as Text
 
 import Data.Text.Markup
 
 propertyTests :: T.TestTree
 propertyTests = T.testGroup "Markup"
-    [ testProperty "test" $ property $ (fromMarkup $ toMarkup "" ()) == [("", ())]
+    [ testProperty "no-op" $ property $ \t ->
+      (fromMarkup $ toMarkup t ()) == [(t, ())]
+
+    , testProperty "text preservation" $ property $ do
+        txt <- arbitrary
+        start <- arbitrary -- `suchThat` (>= 0)
+        len <- arbitrary -- `suchThat` (<= Text.length txt)
+        return $ counterexample (show (txt, start, len)) $
+            let m1 = markRegion start len 2 m0
+                m0 = toMarkup txt 1
+            in (Text.concat $ fst <$> fromMarkup m1) == txt
     ]
 
 main :: IO ()
